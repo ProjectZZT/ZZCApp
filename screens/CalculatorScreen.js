b3c1dfdfbc0f4907cddcbdd3ac4dae333f99ffca
kimhyunjoon <32032047+khj5359@users.noreply.github.com>
2018-07-05 10:11:49 +0900

    단위변환 추가
    단위변환 추가(2018-07-04)

--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
-import { Constants } from 'expo';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, ScrollView } from "react-native";
+import { Constants } from "expo";
 
-import HistoryBox from '../components/HistoryBox';
-import ExpressionBox from '../components/ExpressionBox';
-import ResultBox from '../components/ResultBox.js';
-import NumPad from '../components/NumPad.js';
+import SiUnitsBox from "../components/SiUnitsBox";
+import ExpressionBox from "../components/ExpressionBox";
+import ResultBox from "../components/ResultBox.js";
+import NumPad from "../components/NumPad.js";
 
-import Swiper from 'react-native-swiper';           //swipe modules url:  https://www.npmjs.com/package/react-native-swiper
-  // zerozone calculator CalculatorScreen
+import Swiper from "react-native-swiper"; //swipe modules url:  https://www.npmjs.com/package/react-native-swiper
+// zerozone calculator CalculatorScreen
 
-const math = require('mathjs');
+const math = require("mathjs");
 
 const meter = "(100)";
 const kg = "(200)";
@@ -22,16 +22,16 @@ const cd = "(700)";
 
 export default class CalculatorScreen extends React.Component {
   static navigationOptions = {
-    header: null,//header 부분 감춤
+    header: null //header 부분 감춤
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       lastexpression: [],
-      expression: '',
-      result: ''
-    }
+      expression: "",
+      result: ""
+    };
 
     this._assembleExpression = this._assembleExpression.bind(this);
     this._calculateResult = this._calculateResult.bind(this);
@@ -40,21 +40,23 @@ export default class CalculatorScreen extends React.Component {
   }
 
   _rollbackExpression() {
-    this.state.expression && this.setState((prevState) => ({
-      expression: prevState.lastexpression.pop(), //pop() 배열 끝부분 제거
-      lastexpression: prevState.lastexpression
-    }));
+    this.state.expression &&
+      this.setState(prevState => ({
+        expression: prevState.lastexpression.pop(), //pop() 배열 끝부분 제거
+        lastexpression: prevState.lastexpression
+      }));
   }
 
   _rollbackExpressionAll() {
-    this.state.expression && this.setState((prevState) => ({
-      expression: prevState.lastexpression.splice(), //splice() 배열 전체 부분 초기화
-      lastexpression: prevState.lastexpression
-    }));
+    this.state.expression &&
+      this.setState(prevState => ({
+        expression: prevState.lastexpression.splice(), //splice() 배열 전체 부분 초기화
+        lastexpression: prevState.lastexpression
+      }));
   }
 
   _assembleExpression(symbol) {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       lastexpression: [...prevState.lastexpression, prevState.expression],
       expression: prevState.expression + symbol
     }));
@@ -64,6 +66,7 @@ export default class CalculatorScreen extends React.Component {
     //연산
     let source = this.state.expression.toString();
     let result;
+    let si_m, si_kg, si_s, si_A, si_K, si_mol, si_cd;
     try {
       for (let i = 0; i < this.state.expression.length; i++) {
         if (source.indexOf("mol") != -1) {
@@ -86,71 +89,85 @@ export default class CalculatorScreen extends React.Component {
       }
 
       result = math.eval(source);
+      si_m = result * 100 + "m";
+      si_kg = result * 200 + "kg";
+      si_s = result * 300 + "s";
+      si_A = result * 400 + "A";
+      si_K = result * 500 + "K";
+      si_mol = result * 600 + "mol";
+      si_cd = result * 700 + "cd";
     } catch (e) {
       result = "Error";
     }
     this.setState({ result: result });
+    this.setState({ si_m: si_m });
+    this.setState({ si_kg: si_kg });
+    this.setState({ si_s: si_s });
+    this.setState({ si_A: si_A });
+    this.setState({ si_K: si_K });
+    this.setState({ si_mol: si_mol });
+    this.setState({ si_cd: si_cd });
   }
-
-  
 
   render() {
     return (
-      <Swiper 
-        showsButtons={false} 
-        autoplay={false} 
-        horizontal={false} 
-        bounces={true} 
-        showsPagination={false} 
+      <Swiper
+        showsButtons={false}
+        autoplay={false}
+        bounces={true}
+        showsPagination={false}
         loop={false}
       >
         <View style={styles.slide1}>
           <ExpressionBox expression={this.state.expression} />
-          <ResultBox result={this.state.result}/>
+          <ResultBox result={this.state.result} />
           <NumPad
-              assembleExpression={this._assembleExpression}
-              calculateResult={this._calculateResult}
-              deletePressed={this._rollbackExpression}
-              deletePressedAll={this._rollbackExpressionAll}
-              />
-          <View style={styles.tabBarInfoContainer}> 
+            assembleExpression={this._assembleExpression}
+            calculateResult={this._calculateResult}
+            deletePressed={this._rollbackExpression}
+            deletePressedAll={this._rollbackExpressionAll}
+          />
+          <View style={styles.tabBarInfoContainer}>
             <Text style={styles.tabBarInfoText}>↓ HistoryView ↓</Text>
-          </View>                 
+          </View>
         </View>
         <View style={styles.slide2}>
-          {/* <HistoryBox history={this.state.history}/> */}
-          <Text style={{textAlign: 'center'}}>HistoryBox</Text>
+          <SiUnitsBox
+            si_m={this.state.si_m}
+            si_kg={this.state.si_kg}
+            si_s={this.state.si_s}
+            si_A={this.state.si_A}
+            si_K={this.state.si_K}
+            si_mol={this.state.si_mol}
+            si_cd={this.state.si_cd}
+          />
         </View>
-
       </Swiper>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
   slide1: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   slide2: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 15,
     left: 0,
-    right: 0,
+    right: 0
   },
   tabBarInfoText: {
     fontSize: 15,
-    color: '#2f95dc',
-    textAlign: 'center'
+    color: "#2f95dc",
+    textAlign: "center"
   }
-
 });
